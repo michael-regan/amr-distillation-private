c1e9c072c2e5fb29e6142bfb5257b9404ddd98a4
mr7723 <office@home.local>
2023-08-02 17:21:21 -0700

    add data path as arg

--- src/do-inference-llama.py
+++ src/do-inference-llama.py
@@ -10,6 +10,17 @@ from llama import Llama
 
 
 
+"""
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir llama-2-7b-chat/ \
+    --tokenizer_path tokenizer.model \
+    --max_seq_len 512 \
+    --max_batch_size 4 \
+    --data_path '../portfolio/amr-distillation-private/data/llama-pulley-prompts.json'
+
+
+"""
+
 
 
 def main(
@@ -17,9 +28,10 @@ def main(
     tokenizer_path: str,
     temperature: float = 0.6,
     top_p: float = 0.9,
-    max_seq_len: int = 4000,
+    max_seq_len: int = 1024,
     max_batch_size: int = 4,
     max_gen_len: Optional[int] = None,
+    data_path: str
 ):
     generator = Llama.build(
         ckpt_dir=ckpt_dir,
@@ -28,7 +40,7 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    with open('../data/llama-pulley-prompts.json') as fin:
+    with open() as fin:
         dialogs = json.load(fin)
 
     results = generator.chat_completion(
