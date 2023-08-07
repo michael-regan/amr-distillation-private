5c22b44d55f13f178a5a40b19848b29ff339c8d3
mr7723 <office@home.local>
2023-08-07 16:00:03 -0700

    check how smatch import

--- src/do-inference-llama.py
+++ src/do-inference-llama.py
@@ -16,7 +16,7 @@ from sembleu.src.bleu_score import SmoothingFunction, NgramInst
 # from sembleu.src.amr_graph import AMRGraph
 import math
 
-from smatch import *
+from smatch import smatch_main
 
 import fractions
 try:
@@ -287,7 +287,7 @@ def main(
                     f"> Reference: {thisRef}"
                 )
 
-                smatch_score = smatch.main([thisRef], [thisHyp])
+                smatch_score = smatch_main([thisRef], [thisHyp])
                 print(f">Smatch:{smatch_score}")
                 d['score']=smatch_score
 
