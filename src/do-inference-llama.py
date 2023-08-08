b1eff68ea9819e33587930877de1b8887027b717
mr7723 <office@home.local>
2023-08-08 15:34:59 -0700

    update results dict

--- src/do-inference-llama.py
+++ src/do-inference-llama.py
@@ -32,9 +32,9 @@ torchrun --nproc_per_node 2 do-inference-llama.py \
     --max_seq_len 4096 \
     --max_batch_size 4 \
     --num_chunks 2 \
-    --temperature 1.0 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_2023-08-07.json \
-    --report_path ~/reports/llama-massive-13b-chat_8_exs_2023-08-07.json \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_2023-08-07.json \
+    --report_path ~/reports/llama-massive-13b-chat_16_exs_2023-08-07.json \
     --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-07.jsonl
 
 
