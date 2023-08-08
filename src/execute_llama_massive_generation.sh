041fcebda0d3b6b75ba2306fa8c3efb6a0608a22
mr7723 <office@home.local>
2023-08-08 16:08:42 -0700

    remove 7b models

--- src/execute_llama_massive_generation.sh
+++ src/execute_llama_massive_generation.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
-torchrun --nproc_per_node 1 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_2023-08-07.json \
-    --report_path ~/reports/llama-massive-7b-chat_8_exs_2023-08-07.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 1 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_2023-08-07.json \
-    --report_path ~/reports/llama-massive-7b-chat_12_exs_2023-08-07.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 1 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_2023-08-07.json \
-    --report_path ~/reports/llama-massive-7b-chat_16_exs_2023-08-07.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
 torchrun --nproc_per_node 2 do-inference-llama.py \
     --ckpt_dir ../../../models/llama/llama-2-13b-chat \
     --tokenizer_path ../../../models/llama/tokenizer.model \
