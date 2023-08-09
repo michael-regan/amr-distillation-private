e187e8f545b37e8214c23f148411eb7183e73e85
mr7723 <office@home.local>
2023-08-09 12:47:15 -0700

    for fi de es

--- src/execute_llama_massive_generation.sh
+++ src/execute_llama_massive_generation.sh
@@ -7,94 +7,127 @@ torchrun --nproc_per_node 1 do-inference-llama.py \
     --max_batch_size 4 \
     --num_chunks 2 \
     --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_2023-08-08.json \
-    --report_path ~/reports/llama-massive-7b-chat_8_exs_2023-08-08.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 1 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_2023-08-08.json \
-    --report_path ~/reports/llama-massive-7b-chat_12_exs_2023-08-08.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 1 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_2023-08-08.json \
-    --report_path ~/reports/llama-massive-7b-chat_16_exs_2023-08-08.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 2 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_2023-08-08.json \
-    --report_path ~/reports/llama-massive-13b-chat_8_exs_2023-08-08.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 2 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_2023-08-08.json \
-    --report_path ~/reports/llama-massive-13b-chat_12_exs_2023-08-08.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 2 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
-    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_2023-08-08.json \
-    --report_path ~/reports/llama-massive-13b-chat_16_exs_2023-08-08.json \
-    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
-
-torchrun --nproc_per_node 2 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
-    --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
-    --max_batch_size 4 \
-    --num_chunks 2 \
-    --temperature 0.9 \
     --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_spanish_2023-08-08.json \
-    --report_path ~/reports/llama-massive-13b-chat_8_exs_spanish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_8_exs_spanish_2023-08-08.json \
     --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
 
-torchrun --nproc_per_node 2 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_german_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_8_exs_german_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
     --tokenizer_path ../../../models/llama/tokenizer.model \
     --max_seq_len 4096 \
     --max_batch_size 4 \
     --num_chunks 2 \
     --temperature 0.9 \
     --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_spanish_2023-08-08.json \
-    --report_path ~/reports/llama-massive-13b-chat_12_exs_spanish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_12_exs_spanish_2023-08-08.json \
     --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
 
-torchrun --nproc_per_node 2 do-inference-llama.py \
-    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_german_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_12_exs_german_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
     --tokenizer_path ../../../models/llama/tokenizer.model \
     --max_seq_len 4096 \
     --max_batch_size 4 \
     --num_chunks 2 \
     --temperature 0.9 \
     --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_spanish_2023-08-08.json \
-    --report_path ~/reports/llama-massive-13b-chat_16_exs_spanish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_16_exs_spanish_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_german_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_16_exs_german_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_finnish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_8_exs_finnish_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_finnish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_12_exs_finnish_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-7b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_finnish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-7b-chat_16_exs_finnish_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-8_exs_finnish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-13b-chat_8_exs_finnish_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_finnish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-13b-chat_12_exs_finnish_2023-08-08.json \
+    --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
+
+torchrun --nproc_per_node 1 do-inference-llama.py \
+    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 4096 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-16_exs_finnish_2023-08-08.json \
+    --report_path ~/reports/llama-massive-13b-chat_16_exs_finnish_2023-08-08.json \
     --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
