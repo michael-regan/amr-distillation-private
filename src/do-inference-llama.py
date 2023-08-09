1bd2a6301586afa465899f8f64d1b1096cd728aa
mr7723 <office@home.local>
2023-08-08 18:01:40 -0700

    not sure about jsonlines

--- src/do-inference-llama.py
+++ src/do-inference-llama.py
@@ -29,10 +29,10 @@ from llama import Llama
 torchrun --nproc_per_node 2 do-inference-llama.py \
     --ckpt_dir ../../../models/llama/llama-2-13b-chat \
     --tokenizer_path ../../../models/llama/tokenizer.model \
-    --max_seq_len 4096 \
+    --max_seq_len 2048 \
     --max_batch_size 4 \
     --num_chunks 2 \
-    --temperature 0.4 \
+    --temperature 0.9 \
     --data_path ~/portfolio/amr-distillation-private/data/llama-massive-prompts-12_exs_test_2023-08-08.json \
     --report_path ~/reports/llama-massive-13b-chat_12_exs_test_2023-08-07.json \
     --compiled_results_path ~/reports/llama-massive-compiled-results_2023-08-08.jsonl
@@ -274,7 +274,12 @@ def main(
                     sntbleu = 'Error in literal_eval'
 
                 try:
-                    sntbleu = round(sembleu_script.sentence_bleu([thisRef], thisHypInst, weights=weights, smoothing_function=smoofunc, auto_reweigh=False), max_ngrams)
+                    sntbleu = round(sembleu_script.sentence_bleu([thisRef], 
+                                                                 thisHypInst, 
+                                                                 weights=weights, 
+                                                                 smoothing_function=smoofunc, 
+                                                                 auto_reweigh=False), 
+                                                                 max_ngrams)
                 except Exception as e:
                     print(f"Error in sentence_bleu: {e}")
                     sntbleu = 'Error in sentence_bleu'
@@ -346,12 +351,9 @@ def main(
 
     print(f"Adding compiled results to: {compiled_results_path}")
 
-    write_jsonlines = True
-
-    if write_jsonlines:
-        write_jsonlines = False
-        with jsonlines.open(compiled_results_path, 'a') as writer:
-            writer.write(final_results_dict)
+    write_jsonlines = False
+    with jsonlines.open(compiled_results_path, 'a') as writer:
+        writer.write(final_results_dict)
 
 if __name__ == "__main__":
     fire.Fire(main)
