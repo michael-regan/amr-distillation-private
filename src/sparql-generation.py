1b970d661d42ad9176328b5ac35aa0b2212d917b
mr7723 <office@home.local>
2023-08-14 10:15:13 -0700

    initial commit

---
+++ src/sparql-generation.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+SPARQL generation with LLaMa
+Four formats of input:
+- unconstrained SPARQL relations; Input: query
+- unconstrained SPARQL relationsl; Input: query + AMR 
+- constrained SPARQL relations; Input: query
+- constrained SPARQL relations; Input: query + AMR
+
+"""
+
+"""
+torchrun --nproc_per_node 2 sparql-generation.py \
+    --ckpt_dir ../../../models/llama/llama-2-13b-chat \
+    --tokenizer_path ../../../models/llama/tokenizer.model \
+    --max_seq_len 1024 \
+    --max_batch_size 4 \
+    --num_chunks 2 \
+    --temperature 0.9 \
+    --data_path ~/portfolio/amr-distillation-private/data/llama-sparql-prompts-2023-08-14.json \
+    --report_path ~/reports/sparql-qald9-13b-chat_2023-08-14.json
+"""
+
+import sys
+
+import datetime
+import fire
+import json
+import math
+import numpy as np
+import pandas as pd
+import re
+
+from ast import literal_eval
+
+from llama import Llama
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def main(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    data_path: str,
+    report_path: str,
+    compiled_results_path: str,
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    max_seq_len: int = 4096,
+    max_batch_size: int = 4,
+    num_chunks: int = 4,
+    max_gen_len: Optional[int] = None
+):
+    generator = Llama.build(
+        ckpt_dir=ckpt_dir,
+        tokenizer_path=tokenizer_path,
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size    
+    )
+
+    # Setting DBPedia endpoint
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+
+    with open(data_path, 'r') as fin:
+        messages = json.load(fin)
+
+    theseMessages = [i['messages'] for i in messages]
+
+    theseMessageInstanceChunks = chunks(messages, num_chunks)
+    theseMessageChunks = chunks(theseMessages, num_chunks)
+
+    theseResults = list()
+
+    total_results, total_malformed = 0,0
+
+    for messageInstanceChunk, messageChunk in zip(theseMessageInstanceChunks, theseMessageChunks):
+
+        results = generator.chat_completion(
+            messageChunk,  # type: ignore
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
+        )
+
+        for d, result in zip(messageInstanceChunk, results):
+
+            message = d['message']
+
+            print(f"Question: {d['question']}")
+            print(f"EN Question: {d['en_question']}")
+
+            rc = result['generation']['content'].strip()
+
+            if rc[0]!='{' or rc[-1]!='}':
+                print("Error in generation, cleaning up...")
+                print(rc)
+                if '{' not in rc or '}' not in rc:
+                    d['score'] = 'Error in generation'
+                    d['content'] = result['generation']['content']
+                    d['temperature'] = temperature
+                    theseResults.append(d)
+                    continue
+                else:
+                    rc, sep, tail = rc.partition('}')
+                    rc += '}'
+                    print(rc)
+            
+            try:
+                literal_results = literal_eval(rc)
+
+                hyp_sparql = literal_results['sparql_query']
+                hyp_relations = literal_results['relations']
+                hyp_verification = literal_results['verification']
+
+                ref_sparql = d['gold_sparql']
+    
+                print(
+                    f"> Hypothesis: {hyp_sparql}"
+                )
+                print(
+                    f"> Reference: {ref_sparql}"
+                )
+            except Exception as e:
+                print(f"Error in literal_eval: {e}")
+                error_note = 'Error in literal_eval'
+
+            try:
+                sparql.setQuery(hyp_sparql)
+                sparql_results = sparql.query().convert()
+                pattern = r'<results distinct="false" ordered="true">\s*</results>'
+                match = re.search(pattern, results.toxml())
+
+                if not match:
+                    print("GOT RESULT")
+                    total_results += 1
+
+            except Exception as e:
+                print(f"Malformed query: {e}")
+                total_malformed += 1
+
+            print("\n==================================\n")
+
+            d['content'] = result['generation']['content']
+            d['temperature'] = temperature
+            theseResults.append(d)
+
+    print(f"Total matched results: {total_results}")
+    print(f"Total malformed queries: {total_malformed}")
+    print()
+
+    # print(f"Write report to: {report_path}")
+    # with open(report_path, 'w') as fout:
+    #     json.dump(theseResults, fout, indent=4)
+
+    # # quick view of results
+
+    # language = '-'.join(theseResults[0]['id'].split('-')[1:])
+
+    # ckpt_name = ckpt_dir.split('/')[-1]
+    # final_results_dict = analyze_results(theseResults, language, ckpt_name)
+
+    # print(f"Adding compiled results to: {compiled_results_path}")
+
+    # with jsonlines.open(compiled_results_path, 'a') as writer:
+    #     writer.write(final_results_dict)
+
+if __name__ == "__main__":
+    fire.Fire(main)
