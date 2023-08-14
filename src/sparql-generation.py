7d9b6ddbe7fb64bd963deace5324c3f574e69068
mr7723 <office@home.local>
2023-08-14 15:43:26 -0700

    check if global value persists

--- src/sparql-generation.py
+++ src/sparql-generation.py
@@ -37,7 +37,7 @@ from ast import literal_eval
 
 from llama import Llama
 
-from SPARQLWrapper import SPARQLWrapper, CSV, JSON
+from SPARQLWrapper import SPARQLWrapper, JSON, RDF
 from typing import Optional
 
 
@@ -173,7 +173,7 @@ def main(
 
     valid_dbpedia_props = get_dbpedia_properties()
 
-    sparql.setReturnFormat(RDF)
+    #sparql.setReturnFormat(RDF)
 
     for messageInstanceChunk, messageChunk in zip(theseMessageInstanceChunks, theseMessageChunks):
 
